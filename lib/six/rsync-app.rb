398d09c11941f2cdf339728b961e5cca38352e9e
Sickboy <sb@dev-heaven.net>
2009-09-30 18:16:10 +0200

    Added: Basic application to work with rsync repositories

---
+++ lib/six/rsync-app.rb
@@ -0,0 +1,102 @@
+# encoding: UTF-8
+
+# TODO: Cleanup mess
+
+require 'log4r'
+require 'six/rsync'
+require 'six/rsync/options'
+
+module Six
+  module Repositories
+    module Rsync
+      COMPONENT = 'six-rsync'
+
+      # Create loggers
+      @@log = Log4r::Logger.new(COMPONENT)
+      if defined?(DEBUG)
+        format1 = Log4r::PatternFormatter.new(:pattern => "[%l] %d: %m", :date_pattern => '%H:%M:%S')
+      else
+        format1 = Log4r::PatternFormatter.new(:pattern => "%m")
+      end
+      format2 = Log4r::PatternFormatter.new(:pattern => "[%l] %c %d: %m", :date_pattern => '%H:%M:%S')
+
+      # Create Outputters
+      o_file = Log4r::FileOutputter.new "#{COMPONENT}-file",
+        'level' => 0, # All
+      :filename => "#{COMPONENT}.log",
+        'formatter' =>  format2
+      #:maxsize => 1024
+
+      @@log.outputters << o_file
+
+      o_out = Log4r::StdoutOutputter.new "#{COMPONENT}-stdout",
+        'level' => 2, # no DEBUG
+      'formatter' =>  format1
+
+      o_err = Log4r::StderrOutputter.new "#{COMPONENT}-stderr",
+        'level' => 4, # Error and Up
+      'formatter' =>  format1
+
+      @@log.outputters << o_out << o_err
+
+      module_function
+      def logger
+        @@log
+      end
+
+      class App
+        attr_reader :repo
+        def logger
+          Six::Repositories::Rsync.logger
+        end
+
+        def initialize(folder)
+          @folder = folder
+          @repo = Six::Repositories::Rsync.open(folder, :log => logger)
+        end
+
+        def self.open(folder)
+          app = self.new(folder)
+          app
+        end
+
+        def self.commit(folder)
+          app = self.new(folder)
+          app.repo.commit
+          app
+        end
+
+        def self.update(folder)
+          app = self.new(folder)
+          app.repo.update
+          app
+        end
+
+        def self.clone(host)
+
+        end
+
+        def self.init(folder)
+#          if File.exists?(folder)
+#            logger.error "#{folder} already exists!"
+#            Process.exit
+#          end
+          @repo = Six::Repositories::Rsync.init(folder, :log => Six::Repositories::Rsync.logger)
+        end
+      end
+
+      parse_options
+
+      unless ARGV[0]
+        logger.error "No folder argument given!"
+        Process.exit
+      end
+
+
+      #app = App.new(ARGV[0])
+      @@options.each do |option|
+        App.send option, ARGV[0]
+      end
+    end
+  end
+end
---
+++ lib/six/rsync/options.rb
@@ -0,0 +1,94 @@
+# encoding: UTF-8
+
+# Docu: http://www.ruby-doc.org/stdlib/libdoc/optparse/rdoc/classes/OptionParser.html
+require 'optparse'
+
+module Six
+  module Repositories
+    module Rsync
+      module_function
+      def parse_options
+        todo = [] #, general_todo, second_todo = [], [], []
+
+        options = Hash.new
+        OptionParser.new do |opts|
+          $0[/.*\/(.*)/]
+          opts.banner = "Usage: #{$1} [folder] [options]"
+
+          opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+            options[:verbose] = v
+          end
+
+          opts.on("-i", "--init", "Initializes Repository") do |bool|
+            todo << :init if bool
+          end
+
+          opts.on("-u", "--update", "Updates Repository") do |bool|
+            todo << :update if bool
+          end
+
+          opts.on("-c", "--commit", "Commits changes to Repository") do |bool|
+            todo << :commit if bool
+          end
+
+          opts.on("--clone S", String, "Clones a Repository") do |s|
+            todo << :clone
+            @@host = s
+          end
+
+=begin
+        opts.on("--depth I", Integer, "Clone depth, default: #{@config[:depth]}. Set to 0 to clone all history") do |s|
+          options[:depth] = s
+        end
+
+        opts.on("--mods S", String, "Additional Mods") do |s|
+          options[:mods] = s
+        end
+=end
+        end.parse!
+        @@options = todo
+=begin
+      default = if (todo + second_todo + general_todo).size > 0
+        false
+      else
+        true
+      end
+
+      # TODO: Move this to Updater ?
+      @todo = if todo.size > 0
+        todo
+      else
+        log.info "No parameters given, running the default"
+        #options[:wait] = true
+        if default
+          @config[:defaultactions]
+        else
+          []
+        end
+      end
+      @general_todo = if general_todo.size > 0
+        general_todo
+      else
+        if default
+          @config[:defaultgeneralactions]
+        else
+          []
+        end
+      end
+
+      @second_todo = if second_todo.size > 0
+        second_todo
+      else
+        if default
+          @config[:defaultsecondactions]
+        else
+          []
+        end
+      end
+      @config = @config.merge(options)
+=end
+      end
+    end
+  end
+end
+
